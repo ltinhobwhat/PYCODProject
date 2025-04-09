5adc1fc9334d05cea046725a296ac12d361b8180
Laurent C. <laurentcarlizzi@gmail.com>
2025-04-09 22:42:10 +0200

    Update menu.py to run with blueprint

--- menu.py
+++ menu.py
@@ -1,22 +1,19 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for
 
-app = Flask(__name__)
+menu_bp = Blueprint('menu', __name__)
 
-@app.route('/')
+@menu_bp.route("/")
 def menu():
-    return render_template('menu.html')
+    return render_template("menu.html")
 
-@app.route('/start_game/<int:game_number>')
-def start_game(game_number):
-    return f"Game {game_number} started!"
+@menu_bp.route("/game1")
+def game1():
+    return redirect(url_for("pswd_app.index"))
 
-@app.route('/save_game')
+@menu_bp.route('/save_game')
 def save_game():
     return "Game saved!"
 
-@app.route('/end_game')
+@menu_bp.route('/end_game')
 def end_game():
     return "Game ended!"
-
-if __name__ == '__main__':
-    app.run(debug=True)
