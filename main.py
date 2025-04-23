2e6221d4ff73a4a425645357898963b0125a26c2
loic1911635 <loic152001@gmail.com>
2025-04-24 00:14:33 +0200

    Fix commit

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, redirect, url_for
 from pswdChecker import pswd_app  # Blueprint for password game
 from menu import menu_bp         # Blueprint for menu
 from map import map_bp           # NEW: Blueprint for the map
+from quiz import quiz_bp
+
 
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
@@ -10,6 +12,7 @@ app.secret_key = "supersecretkey"
 app.register_blueprint(menu_bp)
 app.register_blueprint(pswd_app, url_prefix='/pswdChecker')
 app.register_blueprint(map_bp, url_prefix='/map')  # Mount the map at `/map`
+app.register_blueprint(quiz_bp, url_prefix='/quiz')
 
 if __name__ == "__main__":
     app.run(debug=True)
