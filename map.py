8c3a39e68e658043ec0f963a9123a3b7b99a1565
ltinhobwhat <laurentcarlizzi@gmail.com>
2025-05-29 15:47:59 +0200

    Map Integration

--- map.py
+++ map.py
@@ -10,3 +10,23 @@ def map_page():
 @map_bp.route('/minigame/password')
 def launch_password_game():
     return redirect(url_for('pswd_app.index')) 
+
+@map_bp.route('/minigame/quiz')
+def launch_quiz():
+    return redirect(url_for('quiz.index'))
+
+@map_bp.route('/minigame/vigenere')
+def launch_vigenere():
+    return redirect(url_for('vigenere.index'))
+
+@map_bp.route('/minigame/hashgame')
+def launch_hashgame():
+    return redirect(url_for('hashgame.index'))
+
+@map_bp.route('/minigame/sqlinjector')
+def launch_sqlinjector():
+    return redirect(url_for('sqlinjector.index'))
+
+@map_bp.route('/minigame/sqldefender')
+def launch_sqldefender():
+    return redirect(url_for('sqlinjector.defender'))
